7029139a2a207f1adc35ef6d3e66d77c18e02c7a
Ruperth <ruperthnyagesoa@gmail.com>
2023-03-29 10:24:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  //loop through string halfway
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    //compare firstletter with last letter
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
